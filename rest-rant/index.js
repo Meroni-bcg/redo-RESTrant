76f7aa7f007bd26efac1265f2dd389922d31500c
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-02-22 22:36:53 -0800

    call a return 404 status and chain w/ .send

--- rest-rant/index.js
+++ rest-rant/index.js
@@ -17,9 +17,9 @@ app.get('/', (req, res) => {
 })
 
 // 2nd route must be below other route or itll overide. use wildcard *
-
+//return a status code 404 by adding res.status(404)
 app.get("*", (req, res) =>{
-    res.send("<h1> 404 Page </h1>")
+    res.status(404).send("<h1> 404 Page </h1>")
 })
 
 // pull the PORT variable from the .env file
